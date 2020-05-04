cf97de971c0ccc7e9cdb646f6f3d352f8a4a656a
🐹 <@gmail.com>
2020-05-05 02:19:58 +0800

    no message

---
+++ PullToRefreshDemo/DemoTableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  DemoTableViewController.swift
+//  PullToRefreshDemo
+//
+//  Created by dasdom on 17.01.15.
+//  Copyright (c) 2015 Dominik Hauser. All rights reserved.
+//
+
+import UIKit
+
+class DemoTableViewController: UITableViewController {
+  
+  var refreshView: BreakOutToRefreshView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let refreshHeight = CGFloat(100)
+    refreshView = BreakOutToRefreshView(scrollView: tableView)
+    refreshView.delegate = self
+    
+    // configure the refresh view
+//    refreshView.scenebackgroundColor = UIColor(hue: 0.68, saturation: 0.9, brightness: 0.3, alpha: 1.0)
+//    refreshView.paddleColor = UIColor.lightGrayColor()
+//    refreshView.ballColor = UIColor.whiteColor()
+//    refreshView.blockColors = [UIColor(hue: 0.17, saturation: 0.9, brightness: 1.0, alpha: 1.0), UIColor(hue: 0.17, saturation: 0.7, brightness: 1.0, alpha: 1.0), UIColor(hue: 0.17, saturation: 0.5, brightness: 1.0, alpha: 1.0)]
+    
+    tableView.addSubview(refreshView)
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 20
+  }
+  
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("DemoCell", forIndexPath: indexPath) as UITableViewCell
+    
+    cell.textLabel?.text = "Row \(indexPath.row)"
+    
+    return cell
+  }
+  
+}
+
+extension DemoTableViewController: UIScrollViewDelegate {
+ 
+  override func scrollViewDidScroll(scrollView: UIScrollView) {
+    refreshView.scrollViewDidScroll(scrollView)
+  }
+  
+  override func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    refreshView.scrollViewWillEndDragging(scrollView, withVelocity: velocity, targetContentOffset: targetContentOffset)
+  }
+  
+  override func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+    refreshView.scrollViewWillBeginDragging(scrollView)
+  }
+}
+
+extension DemoTableViewController: BreakOutToRefreshDelegate {
+  
+  func refreshViewDidRefresh(refreshView: BreakOutToRefreshView) {
+    // load stuff from the internet
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_SEC * 3)), dispatch_get_main_queue(), { () -> Void in
+      refreshView.endRefreshing()
+    })
+  }
+
+}
